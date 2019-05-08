5d51364bc3667fa3e1d137b5d517ff4c5d2606ff
Tamir Adler <tamiradler@gmail.com>
2019-05-08 13:35:28 +0300

    fixing sign in

--- clnt/src/app/sign-in/sign-in.component.ts
+++ clnt/src/app/sign-in/sign-in.component.ts
@@ -13,66 +13,28 @@ export class SignInComponent implements OnInit, AfterViewInit {
 
   isSignIn: boolean = false;
 
-  private clientId:string = '795668388432-7lor70m45089bapr51bng6t14003g4hc.apps.googleusercontent.com';
-
-  private scope = [
-    'profile',
-    'email'
-  ].join(' ');
-
-  public auth2: any;
-
-  public googleInit() {
-    let that = this;
-    gapi.load('auth2', function () {
-      that.auth2 = gapi.auth2.init({
-        client_id: that.clientId,
-        cookiepolicy: 'single_host_origin',
-        scope: that.scope
-      });
-      that.attachSignin(document.getElementById("googleBtn"));
-    });
-  }
-
-  public attachSignin(element) {
-    let that = this;
-    this.auth2.attachClickHandler(element, {},
-      googleUser => {
-        this.ngZone.run(()=>{
-          that.isSignIn = true;
-          this.userService.unsetUser();
-        });
-        
-        let profile = googleUser.getBasicProfile();
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('ID: ' + profile.getId());
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-      }, error => {
-        console.log(JSON.stringify(error, undefined, 2));
-      });
-  }
-
   constructor(private userService: UserService, private ngZone: NgZone) {
-    
+    this.userService.user$.subscribe(user=>
+      {
+        ngZone.run(()=>{
+          if (user==null)
+          {
+            this.isSignIn = false;
+          } else {
+            this.isSignIn = true;
+          }
+        });
+      })
   }
 
   ngOnInit() {
   }
 
-  signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(() => {
-      this.ngZone.run(()=>{
-        this.isSignIn = false;
-        this.userService.unsetUser();
-      });
-      console.log('User signed out.');
-    });
+  ngAfterViewInit(){
+    this.userService.googleInit(document.getElementById("googleBtn"));
   }
 
-  ngAfterViewInit(){
-    this.googleInit();
+  public signOut() {
+    this.userService.signOut();
   }
 }
--- clnt/src/app/user.service.ts
+++ clnt/src/app/user.service.ts
@@ -1,5 +1,6 @@
+declare const gapi: any;
+
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
 import { User } from './user';
 
@@ -7,6 +8,15 @@ import { User } from './user';
   providedIn: 'root'
 })
 export class UserService {
+
+  private auth2: any;
+
+  private scope = [
+    'profile',
+    'email'
+  ].join(' ');
+
+  private clientId:string = '795668388432-7lor70m45089bapr51bng6t14003g4hc.apps.googleusercontent.com';
 
   public user$: BehaviorSubject<User>;
 
@@ -22,4 +32,54 @@ export class UserService {
     this.user$.next(null);
   }
 
+  public googleInit(element: HTMLElement) {
+    let that = this;
+    gapi.load('auth2', () => {
+      gapi.auth2.init({
+        client_id: that.clientId,
+        cookiepolicy: 'single_host_origin',
+        scope: that.scope
+      }).then(()=>{
+        this.auth2 = gapi.auth2.getAuthInstance();
+        if (element != undefined) {
+          that.attachSignin(element);
+        }
+        if (this.auth2.isSignedIn.get()) {
+          var googleUser = this.auth2.currentUser.get()
+          this.handleGoogleUser(googleUser);
+        }
+      });
+    });
+  }
+
+  private handleGoogleUser(googleUser: any) {
+    let profile = googleUser.getBasicProfile();
+    let user: User = {
+      email: profile.getEmail(),
+      familyName: profile.getFamilyName(),
+      givenName: profile.getGivenName(),
+      id: profile.getId(),
+      imageUrl: profile.getImageUrl(),
+      name: profile.getName(),
+      tokenId: googleUser.getAuthResponse().id_token
+    };
+    this.setUser(user);
+  }
+
+  private attachSignin(element: HTMLElement) {
+    this.auth2.attachClickHandler(element, {},
+      googleUser => {
+        this.handleGoogleUser(googleUser);
+      }, error => {
+        console.log(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+  public signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(() => {
+      this.unsetUser();
+      console.log('User signed out.');
+    });
+  }
 }
