31c3a0d049b3e79c71fb20dcc3ec62bfc55224f9
Tamir Adler <tamiradler@gmail.com>
2019-05-24 10:59:49 +0300

    show menu improvment

--- clnt/src/app/show-menu/show-menu.component.ts
+++ clnt/src/app/show-menu/show-menu.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { NavbarToggleService } from '../navbar/navbar.toggle.service';
+import { LoadSpinnerService } from '../load-spinner/load-spinner-service';
+import { MenuRestControllerService } from 'src/swaggergenerate/services';
+import { Menu } from 'src/swaggergenerate/models';
 
 @Component({
   selector: 'app-show-menu',
@@ -14,15 +17,21 @@ export class ShowMenuComponent implements OnInit, AfterViewInit, OnDestroy {
   ngAfterViewInit(): void {
     let div = document.getElementById('contentWithImage');
     div.style.backgroundImage = "url(" + localStorage.getItem('imageResult') + ")";
+    this.contentColor = localStorage.getItem('contentColor');
+    this.headlineColor = localStorage.getItem('headlineColor');
   }
 
+  menu: Menu;
   headlineColor: String;
   contentColor: String;
 
-  constructor(private navbarToggleService: NavbarToggleService) { }
+  constructor(private navbarToggleService: NavbarToggleService,
+              private loadSpinnerService: LoadSpinnerService,
+              private menuRestControllerService: MenuRestControllerService) { }
 
   ngOnInit() {
     this.navbarToggleService.data.isToShowNavBar = false;
+    this.loadMenu(new Date());
   }
 
   previewFile(event) {
@@ -40,13 +49,28 @@ export class ShowMenuComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   headlineColorChange(event) {
-    console.log(event.target.value);
+    localStorage.setItem('headlineColor', event.target.value);
     this.headlineColor = event.target.value;
   }
 
   contentColorChange(event) {
-    console.log(event.target.value);
+    localStorage.setItem('contentColor', event.target.value);
     this.contentColor = event.target.value;
   }
 
+  loadMenu(date: Date) {
+    let dateKey: string = date.getDate() + '_' + (date.getMonth()+1) + '_' + date.getFullYear();
+    
+    this.loadSpinnerService.addRequestor('loadMenu');
+    this.menuRestControllerService.getMenusUsingGET(dateKey).subscribe(
+      menu => {
+        this.loadSpinnerService.removeRequestor('loadMenu');
+        this.menu = menu;
+        this.menu.dishs.splice(2, 0, {});
+      },
+      error => {
+        this.loadSpinnerService.removeRequestor('loadMenu');
+      }
+    )
+  }
 }
