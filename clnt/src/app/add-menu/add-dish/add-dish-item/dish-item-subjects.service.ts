82af514758560b7bceb8cd528382453fdc6bee58
Tamir Adler <tamiradler@gmail.com>
2019-06-04 21:58:47 +0300

    improve adding menu

--- clnt/src/app/add-menu/add-dish/add-dish-item/dish-item-subjects.service.ts
+++ clnt/src/app/add-menu/add-dish/add-dish-item/dish-item-subjects.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MenuRestControllerService } from 'src/swaggergenerate/services';
 import { BehaviorSubject } from 'rxjs';
+import { DishItemSubjectCollectionRestControllerService } from 'src/swaggergenerate/services/dish-item-subject-collection-rest-controller.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class DishItemSubjectsService {
 
   subjects$: BehaviorSubject<string[]> = new BehaviorSubject(null);
 
-  constructor(private menuRestControllerService: MenuRestControllerService) {
-    menuRestControllerService.getDistinctDishesUsingGET().subscribe(subjects=>{
+  constructor(private dishItemSubjectCollectionRestControllerService: DishItemSubjectCollectionRestControllerService) {
+    dishItemSubjectCollectionRestControllerService.getDishItemSubjectsUsingGET().subscribe(subjects=>{
       this.subjects$.next(subjects);
     })
   }
