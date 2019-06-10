8e71b3326828a9a2f830bf87af6c4cbaad9355d4
Adler, Tamir (ta076w) <ta076w@att.com>
2019-06-10 14:37:03 +0300

    refactoring

--- srvr/src/main/java/malka/plus/service/DishItemSubjectCollectionService.java
+++ srvr/src/main/java/malka/plus/service/DishItemSubjectCollectionService.java
@@ -1,13 +1,12 @@
 package malka.plus.service;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import malka.plus.model.DishItem;
 import malka.plus.model.DishItemSubjectCollection;
 import malka.plus.repository.DishItemSubjectCollectionRepository;
 import malka.plus.view.Menu;
@@ -21,14 +20,15 @@ public class DishItemSubjectCollectionService
 	public void saveSubjects(Menu menu)
 	{
 		Set <String> subjects = new HashSet<>();
-		List<malka.plus.model.Dish> dishesModels = menu.getDishs();
-		if(dishesModels != null)
-		{
-			dishesModels.forEach(dish -> {
-				List<DishItem> dishItems = dish.getDishItems();
-				dishItems.forEach(dishItem -> subjects.add(dishItem.getSubject()));
-			});
-		}
+
+		Stream.of(menu)
+			.filter(m -> m.getDishs() != null)
+			.flatMap(m -> m.getDishs().stream())
+			.filter(dish -> dish.getDishItems() != null)
+			.flatMap(dish -> dish.getDishItems().stream())
+			.map(dishItem -> dishItem.getSubject())
+			.forEach(subjects::add);
+		
 		DishItemSubjectCollection dishItemSubjectCollection = dishItemSubjectCollectionRepository.findById("1").orElse(new DishItemSubjectCollection());
 		dishItemSubjectCollection.addAllSubjects(subjects);
 		dishItemSubjectCollectionRepository.save(dishItemSubjectCollection);
